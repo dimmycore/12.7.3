366b38a5e26976c47ca000982e964efaa3d57320
dimmycore <112955613+dimmycore@users.noreply.github.com>
2022-11-13 16:46:07 +0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,11 @@
+def bi_search(a: int, array: list) -> int:
+    left, right = 0, len(array)
+    while left < right:
+        middle = (left + right) // 2
+        if array[middle] < a:
+            left = middle + 1
+        else:
+            right = middle
+    return left
+
+print(bi_search(6, [1,2,3,4,5,6,8,12,15,18]))
